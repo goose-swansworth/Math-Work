acfecc7b6d4477784ab05825783e13af8c157d99
goose_swansworth <fridgeineyes@gmail.com>
2022-05-18 15:05:45 +1200

    fixed

--- Math353/assignment2.py
+++ Math353/assignment2.py
@@ -33,12 +33,13 @@ def heun_method(f, h, t0, t_final, y0, args, n):
     w = np.zeros((len(t), n))
     w[0] = y0
     for i in range(len(t) - 1):
+        print(f"t = {t[i]}")
         w_tilde = w[i] + h * f(w[i], t[i], *args)
         w[i + 1] = w[i] + h/2 * (f(w[i], t[i], *args) + f(w_tilde, t[i + 1], *args))
     return t, w
 
 def SEIR_function(w, t, lambd, beta0, beta1, gamma, sigma):
-    """Dirivative function used in SEIR model"""
+    """Derivative function used in SEIR model"""
     S, E, I = w
     beta = lambda t: beta0 * (1 + beta1 * np.cos(2*np.pi*t))
 
@@ -53,9 +54,9 @@ def part_1():
     lambd, beta0, beta1, gamma, sigma = 0.02, 1250, 0, 73, 45.625
     h = 10**-4
     w0 = [0.9, 0.05, 0.05]
-    t0, tf = 0, 100
+    t0, tf = 0, 0.1
     args = (lambd, beta0, beta1, gamma, sigma)
-    w, t = heun_method(SEIR_function, h, t0, tf, y0, args, 3)
+    t, w = heun_method(SEIR_function, h, t0, tf, w0, args, 3)
     S, E, I = w[:, 0], w[:, 1], w[:, 2]
     plot_values([(t, S, "S(t)"), (t, E, "E(t)"), (t, I, "I(t)")], xlabel="t")
     plt.show()
