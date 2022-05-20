a60f3bb10ed45594e62f5d172117bf37056d5cd5
goose_swansworth <fridgeineyes@gmail.com>
2022-05-20 18:36:40 +1200

    Part 3 started

--- Math353/assignment2.py
+++ Math353/assignment2.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from  numpy.linalg import solve
 import math as m
+from math353 import plot_values
 
 plt.rcParams['font.family'] = 'serif'
 plt.rcParams['mathtext.fontset'] = 'cm'
@@ -67,20 +69,20 @@ def read_soln_file(filename):
 
 def part_1():
     """Create required plots for part 1 of assignment"""
-    lambd, beta0, beta1, gamma, sigma = 0.02, 1250, 0.2, 73, 45.625
+    lambd, beta0, beta1, gamma, sigma = 0.02, 1250, 0, 73, 45.625
     h = 10**-4
     w0 = [0.9, 0.05, 0.05]
     t0, tf = 0, 100
     args = (lambd, beta0, beta1, gamma, sigma)
     #t, w = heun_method(SEIR_function, h, t0, tf, w0, args, 3)
 
-    #write_soln_file("part1_c.txt", t, w)
+    #write_soln_file("part1_a.txt", t, w)
 
     S, E, I = read_soln_file("part1_c.txt")
     t = np.arange(t0, tf + h, h)
     index = m.floor(len(t) * 0.6)
 
-    plot_values([(t, S, "S(t)"), (t, E, "E(t)"), (t, I, "I(t)")], xlabel="t", xlim=(0,1))
+    plot_values([(t, S, "S(t)"), (t, E, "E(t)"), (t, I, "I(t)")], xlabel="t")
     plt.grid()
     plt.show()
     axes = plt.figure().add_subplot(projection='3d')
@@ -92,6 +94,59 @@ def part_1():
     plt.show()
 
 
-part_1()
+def part_2():
+    u = lambda x: np.sin(2*x)
+    u_prime = lambda x: 2*np.cos(2*x)
+    diff_func = lambda u, x, h: (u(x - 2*h) -4*u(x - h) + 3*u(x)) / (2*h)
+
+    h_values = np.logspace(-1, -15, 10)
+    error = np.zeros(len(h_values))
+
+    for i in range(len(h_values)):
+        error[i] = abs(u_prime(1) - diff_func(u, 1, h_values[i]))
+
+    plot_values([(np.log(h_values), np.log(error), "")], xlabel="h", ylabel=r"$|u'(1) - \tilde{u}'(1)}|$")
+    plt.show()
+
+def build_matrix(n, h, b):
+    A = np.zeros((n-1, n-1))
+    for i in range(n - 1):
+        xi = (i + 1)*h
+        if i == 0:
+            A[i, i] = -4
+            A[i, i+1] = 2 - h*b(xi)
+        elif i == n - 2:
+            A[i, n-3] = 2 + h*b(xi)
+            A[i, n-2] = -4
+        else:
+            A[i, i-1] = 2 + h*b(xi)
+            A[i, i] = -4
+            A[i, i+1] = 2 - h*b(xi)
+    return A
 
 
+def build_rhs_vector(n, h, a, b):
+    B = np.zeros((n - 1, 1))
+    for i in range(n - 1):
+        xi = (i + 1)*h
+        if i == 0:
+            B[i] = (2*h**2)*a(xi) - (2+h*b(xi))
+        elif i == n - 2:
+            B[i] = (2*h**2)*a(xi) - (2-h*b(xi))
+        else:
+            B[i] = (2*h**2)*a(xi)
+    return B
+
+
+
+def part_3():
+    n =  6#round(1 / 0.01)
+    s = lambda x: 1
+    s_prime = lambda x: 0
+    a = lambda x: (-6*s_prime(x)) / (s(x))**3
+    b = lambda x: (-3*s_prime(x)) / (s(x))
+    A = build_matrix(n, 0.01, b)
+    B = build_rhs_vector(n, 0.01, a, b)
+    print(solve(A, B))
+    
+part_3()
